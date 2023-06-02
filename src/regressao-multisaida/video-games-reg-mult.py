bd2bd63e5f16329733bfee46a8888f66ab967658
Douglas Amorim <douglas@gavea.com>
2023-06-02 09:36:58 -0300

    preprocessamento II

--- src/regressao-multisaida/video-games-reg-mult.py
+++ src/regressao-multisaida/video-games-reg-mult.py
@@ -2,6 +2,8 @@ import pandas as pd
 import os
 from keras.layers import Dense, Dropout, Activation, Input
 from keras.models import Model
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 from rich import print
 
 PATH_DATA = os.path.join(os.path.dirname(__file__),'data')
@@ -25,7 +27,6 @@ def shape_view(title : str, dataframe):
     columns = shape[1]
     print(f'Titulo: {title} || Registros: {records} || Colunas: {columns}')
 
-
 ## removendo colunas desnecessárias
 colunas = ['Other_Sales', 'Global_Sales', 'Developer', 'Name']
 base = drop_columns(colunas)
@@ -41,3 +42,28 @@ shape_view('Removendo linhas NaN', base)
 base = base.loc[base['NA_Sales'] > 1]
 base = base.loc[base['EU_Sales'] > 1]
 shape_view('Removendo vendas menores que 1', base)
+
+# sparando a base entre previsores e classes
+previsores = base.iloc[:, [0, 1, 2, 3, 7, 8 , 9, 10, 11]].values
+
+venda_na = base.iloc[:, 4].values
+venda_eu = base.iloc[:, 5].values
+venda_jp = base.iloc[:, 6].values
+
+# transformar atributos categoricos para numeros
+print(previsores[0])
+le = LabelEncoder()
+colunas_categoricas = [0, 2, 3, 8]
+for idx in colunas_categoricas:
+    previsores[:, idx] = le.fit_transform(previsores[:, idx])
+
+print(previsores[0])
+
+onehotencoder = ColumnTransformer(
+    transformers=[
+        ("OneHot", OneHotEncoder(), colunas_categoricas)
+        ],
+    remainder='passthrough'
+)
+previsores = onehotencoder.fit_transform(previsores).toarray()
+print(len(previsores[0]))
